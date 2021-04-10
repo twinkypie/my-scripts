4b9726a70e5786c0fc49a34faff7407803be2318
Daniel <padillaortiz.daniel099@gmail.com>
2021-04-10 15:04:08 -0700

    devops-linux
    Linux Lab Requirements
    Setup 1 Linux Machine or Server using Terraform or the AWS console
    Install Apache - using a bash script or shell script
    My SQL - using a bash script or shell script
    Importing a database - using a bash script or shell script - https://drive.google.com/file/d/16keAvdBguWIO3TqdQJkVhiGp4FtzNNI5/view?usp=sharing 
    Configuring Apache and mirror a common website
    Add an additional disk to the linux box and use fdisk to partition it.
    add my ssh key to the linux box so that I can login to it remotely.
    Submit all your code for review to me
    
    tasks:
    import the db to ec2
    Configuring Apache and mirror a common website
    
    Goal:
    do the terraform file isntead of creating the instance via gui

---
+++ linux-vm-setup.sh
@@ -0,0 +1,18 @@
+echo "update and upgrade packages"
+sudo apt update -y
+sudo apt upgrade -y
+echo "install apache2"
+sudo apt install apache2 -y
+echo "start apache2 service"
+sudo service apache2 start
+echo "install mysql"
+sudo apt install mysql-server -y
+echo "start msql service"
+sudo service mysql start
+
+
+echo "mount the 10gb drive"
+sudo mount /dev/xvdf1/ /mnt/extra-d
+
+echo"cat into file located in the mounted drive"
+cat /mnt/extra-d/test.txt
