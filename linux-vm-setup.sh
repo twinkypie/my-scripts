1e5daa6b72019f56266f2fa33a203e420a067314
Daniel <padillaortiz.daniel099@gmail.com>
2021-04-17 14:54:05 -0700

    mirror website complete
    Added the code to mirror website
    changed the default html file to the mirrored html file
    linux assignment complete 
    To Do:
    complete windows setup and update powershell script

--- linux-vm-setup.sh
+++ linux-vm-setup.sh
@@ -6,9 +6,29 @@ sudo apt upgrade -y
 #--- install/starting Apache2 server and mirroring website
 echo "install apache2"
 sudo apt install apache2 -y
+
 echo "start apache2 service"
 sudo service apache2 start
-#to-do: still need to mirror website
+
+echo "mirroring a website"
+# creating a folder for the website mirroring
+mkdir web-mirror && cd web-mirror
+# website mirrored
+wget -rpknp https://en.wikipedia.org/wiki/Khabib_Nurmagomedov
+# changing the index file in the default apache2 folder
+cd /var/www/html/
+sudo mv index.html index-old.html
+# the en.wikipedia.org fodler was created by the wget command ran earlier
+sudo cp -r ~/web-mirror/en.wikipedia.org/* .
+# wiki is a folder in en.wikipedia.org; the file Khabib_Nurmagomedov is from the wget (also an html file)
+sudo cp wiki/Khabib_Nurmagomedov .
+# renaming Khabib_Nurmagomedov to index.html
+sudo mv Khabib_Nurmagomedov index.html
+
+echo "restarting apache2" 
+sudo service apache2 restart
+
+echo "mirror of the website should be available type in ip address in web browser"
 
 #--- Installing/starting the mysql server and importing the database---
 echo "install mysql"
@@ -28,4 +48,3 @@ sudo mount /dev/xvdf1/ /mnt/extra-d
 
 echo"cat into file located in the mounted drive"
 cat /mnt/extra-d/test.txt
-
