70760a2e2c9535a438e3a503322877136b0c768e
Daniel <padillaortiz.daniel099@gmail.com>
2021-07-10 14:28:54 -0700

    adding comments to code

--- dpadilla-saas-lab.ps1
+++ dpadilla-saas-lab.ps1
@@ -1,3 +1,4 @@
+# Creating AD groups (different from local groups)
 New-ADGroup -Name "Human Resources" -SamAccountName HR -DisplayName "HR"
 New-ADGroup -Name "Information Technology" -SamAccountName IT -DisplayName "IT"
 New-ADGroup -Name "Finance" -SamAccountName Finance -DisplayName "Finance"
@@ -6,7 +7,7 @@ New-ADGroup -Name "Sales" -SamAccountName Sales -DisplayName "Sales"
 New-ADGroup -Name "Security" -SamAccountName Security -DisplayName "Security"
 
 
-
+# Creating AD account (different from local accounts)
 New-ADUser -Name "Han Solo" -GivenName "Han" -Surname "Solo" -SamAccountName hsolo -Title "HR Assistant" -Department "HR" 
 New-ADUser -Name "Luke Skywalker" -GivenName "Luke" -Surname "Skywalker" -SamAccountName lskywalker -Title "IT Manager" -Department "IT" 
 New-ADUser -Name "Leia Organa" -GivenName "Leia" -Surname "Organa" -SamAccountName lorgana -Title "Financial Advisor" -Department "Finance" 
@@ -14,6 +15,7 @@ New-ADUser -Name "Lando Calrissian" -GivenName "Lando" -Surname "Calrissian" -Sa
 New-ADUser -Name "Sheev Palpatine" -GivenName "Sheev" -Surname "Palpatine" -SamAccountName spalpatine -Title "Sales Agent" -Department "Sales" 
 New-ADUser -Name "Darth Vader" -GivenName "Darth" -Surname "Vader" -SamAccountName dvader -Title "Security Executive" -Department "Security" 
 
+#adding members to corresponding groups 
 Add-AdGroupMember -Identity HR -Members hsolo
 Add-AdGroupMember -Identity IT -Members lskywalker
 Add-AdGroupMember -Identity Finance -Members lorgana
